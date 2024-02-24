8978e2f75f6014d82877ecc518009dd5819d7471
Jessie0611 <ImJustMe0611@hotmail.com>
2024-02-24 14:09:37 -0500

    Add project pages, set up login design, set up acc pg design

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,13 +36,15 @@ android {
 }
 
 dependencies {
+    val nav_version = "2.7.6"
+    implementation ("androidx.navigation:navigation-fragment-ktx:$nav_version")
+    implementation ("androidx.navigation:navigation-ui-ktx:$nav_version")
     implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.fragment:fragment-ktx:1.3.6")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.3.5+")
-    implementation("androidx.navigation:navigation-ui-ktx:2.3.5")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.6")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/hfad/florafolio/HomeFragment.kt
+++ app/src/main/java/com/hfad/florafolio/HomeFragment.kt
@@ -1,19 +1,49 @@
 package com.hfad.florafolio
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
-class HomeFragment : Fragment() {
+import androidx.navigation.Navigation
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+class HomeFragment : Fragment(R.layout.fragment_home) {
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val navController =
+            Navigation.findNavController(requireView())
+
+        when (item.itemId) {
+            R.id.HomeFragment -> {
+                navController.navigate(R.id.HomeFragment)
+                return true
+            }
+
+            R.id.AccountFragment -> {
+                navController.navigate(R.id.AccountFragment)
+                return true
+            }
+
+            R.id.signupFragment -> {
+                navController.navigate(R.id.signupFragment)
+                return true
+            }
+
+            R.id.RoseFragment -> {
+                navController.navigate(R.id.RoseFragment)
+                return true
+            }
+
+            R.id.LavenderFragment -> {
+                navController.navigate(R.id.LavenderFragment)
+                return true
+            }
+
+            else -> return super.onOptionsItemSelected(item)
+        }
     }
-
 }
