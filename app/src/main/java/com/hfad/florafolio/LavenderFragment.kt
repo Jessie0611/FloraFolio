577e75bc9469f2684f612a1cc27fd4988965ab75
Jessie0611 <ImJustMe0611@hotmail.com>
2024-02-26 14:33:19 -0500

    Add project pages, set up login design, set up acc pg design

--- app/src/main/java/com/hfad/florafolio/LavenderFragment.kt
+++ app/src/main/java/com/hfad/florafolio/LavenderFragment.kt
@@ -4,15 +4,30 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
+import com.google.android.material.appbar.AppBarLayout
+import com.google.android.material.appbar.CollapsingToolbarLayout
+import com.google.android.material.appbar.MaterialToolbar
 
 class LavenderFragment : Fragment() {
+        private lateinit var toolbar: MaterialToolbar
+        private lateinit var imageView: ImageView
+        override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+        ): View? {
+            val view = inflater.inflate(R.layout.fragment_lavender, container, false)
+            toolbar = view.findViewById(R.id.toolbar)
+            imageView = view.findViewById(R.id.imageViewLav)
+            val collapsingToolbarLayout: CollapsingToolbarLayout = view.findViewById(R.id.collapsingToolbarLayout)
+            val appBarLayout: AppBarLayout = view.findViewById(R.id.appBarLayout)
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_lavender, container, false)
+            collapsingToolbarLayout.title = "Lavender"
+            appBarLayout.addOnOffsetChangedListener(AppBarLayout.OnOffsetChangedListener { _, verticalOffset ->
+                // Handle toolbar behavior if needed
+            })
+
+            return view
+        }
     }
-}
